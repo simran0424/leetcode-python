7a0917ffa1c90c4b0ccae69c51d51650b00fe83b
simran0424 <simran.jadhav245@gmail.com>
2025-12-26 14:58:57 +0530

    Add solution for leetcode 412: Fizz- buzz Problem

---
+++ easy/lc_412_fizz_buzz.py
@@ -0,0 +1,18 @@
+# LeetCode 412: Fizz Buzz
+# Difficulty: Easy
+
+
+class Solution:
+    def fizzBuzz(self, n: int) -> list[str]:
+        ans = []
+        for i in range(1, n+1):
+            if i%3==0 and i%5==0:
+                ans.append("FizzBuzz")
+            elif i%3==0:
+                ans.append("Fizz")
+            elif i%5==0:
+                ans.append("Buzz")
+            else:
+                ans.append(str(i))
+
+        return ans
